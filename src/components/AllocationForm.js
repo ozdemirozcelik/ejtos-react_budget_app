0cbd8bb4758526ac6dfc19b97b18fb722216036d
ozdemirozcelik <ozdemirozcelik@gmail.com>
2023-04-11 18:45:41 -0400

    final

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -3,7 +3,7 @@ import { AppContext } from '../context/AppContext';
 
 
 const AllocationForm = (props) => {
-    const { dispatch,remaining  } = useContext(AppContext);
+    const { dispatch,remaining, currency  } = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
@@ -72,13 +72,16 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                  
+                  <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
+                <label className="input-group-text" htmlFor="cost">{currency}</label>
+                  </div>
                     <input
                         required='required'
                         type='number'
                         id='cost'
                         value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
+                        // style={{ marginLeft: '2rem' , size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
 
